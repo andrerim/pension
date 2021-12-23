fb2d82be927211be846557e63c2a675ebae347da
andrerim <andreasrim@gmail.com>
2021-12-23 15:07:10 +0200

    Add date picker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
+  const [endDate, setEndDate] = useState(new Date());
+  const [daysUntil, setDaysUntil] = useState(0);
+
+  useEffect(() => {
+    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    const days = Math.round(Math.abs((Date.now() - endDate) / oneDay));
+    setDaysUntil(days);
+  }, [endDate]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning React!
-        </a>
+        <div>
+          <DatePicker
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+          />
+        </div>
       </header>
+
+      <p>Days til date: {daysUntil}</p>
     </div>
   );
 }
